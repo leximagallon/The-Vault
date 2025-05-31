cd9556c96feb3f173d4d13de2ccd532cdd18d9a1
leximagallon <amagallon0779@sdsu.edu>
2025-05-31 11:44:22 -0700

    update index.html and index.js with vault code

---
+++ index.js
@@ -0,0 +1,23 @@
+
+let code1; 
+code1 = 7 +3;
+console.log(code1);
+
+let code2;
+code2 = 50- 20 / 2;
+console.log(code2);
+
+let code3;
+code3 = 79 % 40;
+console.log(code3);
+let message = "the vault has been secured. The combination is";
+
+
+let codeA;
+codeA =  code1 + "-" + code2 + "-" + code3;
+
+let codeB = `${code1}-${code2}-${code3}`
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
